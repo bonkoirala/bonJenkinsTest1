e6948ee5b88027e3efb7cd323eb19e53512801ea
Bonish Koirala <bonish.koirala@ambarkaar.com>
2020-05-12 14:52:46 +0545

    Beginner hello world files

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        String A = "Hello ";
+        String B  = "World";
+        System.out.println(A + B);
+        System.out.println("This will be printed");
+        System.out.println("This will also be printed");
+    }
+}
